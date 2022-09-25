dca1e7bdd8e5da894f077ed49f6c82cc9d001498
Helen Nikitina <helenasandeya@gmail.com>
2022-09-25 16:52:15 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿try
+{
+    int colums = 0;
+    while (colums % 2 == 0)
+    {
+        Console.WriteLine("Enter number of colums:");
+        colums = int.Parse(s: Console.ReadLine());
+    }
+    
+    int rows = colums;
+	int centre = Math.Abs((colums + 1) / 2);
+	for (int i = 1; i <= colums; i++)
+	{
+		if (i<=centre)
+		{
+			for (int j = 1; j <= i-1; j++)
+			{
+				Console.Write(" ");
+			}
+			for (int k = 1; k <= colums + 1 - (2 * i - 1); k++)
+			{
+				Console.Write("*");
+			}
+			Console.WriteLine();
+		}
+		else
+		{
+			for (int j = 1; j <=(colums-centre)-(i-centre); j++)
+			{
+				Console.Write(" ");
+			}
+			for (int k = 1; k <= 2*(i-centre)+1; k++)
+			{
+                Console.Write("*");
+            }
+			Console.WriteLine();
+		}
+
+	}
+}
+catch (Exception)
+{
+
+	Console.WriteLine("Enter only numbers!!!");
+}
